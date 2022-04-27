a1ffef7c64b5bf33e75226248199449fc930fe9c
Allen Lee <allenleexxx@163.com>
2022-04-27 19:39:41 +0800

    初步实现五等奖推荐算法

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 import csv
-
+import random
 
 # 全局变量
 CSV_IN = "data/data.csv"
@@ -11,6 +11,9 @@ MAX_BLUE_NUM = 16
 RED_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
                12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
                23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
+COL_NAME_RED = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
+                "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22",
+                "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33"]
 BLUE_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 
@@ -34,6 +37,136 @@ def log(msg):
     print("")
 
 
-def print_arr(arr):
-    for row in arr:
+def print_r(a, b, c, d, e, f):
+    print("{:0>2d}".format(a), "{:0>2d}".format(b), "{:0>2d}".format(c),
+          "{:0>2d}".format(d), "{:0>2d}".format(e), "{:0>2d}".format(f))
+
+
+class Node:
+    def __init__(self, key, val, prev=None, succ=None):
+        self.key = key
+        self.val = val
+        # 前驱
+        self.prev = prev
+        # 后继
+        self.succ = succ
+
+    def __repr__(self):
+        return str(self.val)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node(None, 'header')
+        self.tail = Node(None, 'tail')
+        self.head.succ = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+        self.str = ""
+
+    def append(self, node):
+        # 将node节点添加在链表尾部
+        prev = self.tail.prev
+        node.prev = prev
+        node.succ = prev.succ
+        prev.succ = node
+        node.succ.prev = node
+        self.size += 1
+
+    def delete(self, node):
+        # 删除节点
+        prev = node.prev
+        succ = node.succ
+        succ.prev, prev.succ = prev, succ
+        self.size -= 1
+
+    def get_list(self):
+        # 返回一个包含所有节点的list，方便上游遍历
+        ret = []
+        cur = self.head.succ
+        while cur != self.tail:
+            ret.append(cur)
+            cur = cur.succ
+        return ret
+
+    def get_by_key(self, key):
+        cur = self.head.succ
+        while cur != self.tail:
+            if cur.key == key:
+                return cur
+            cur = cur.succ
+        return None
+
+    # def to_string(self):
+    #     for node in self.get_list():
+    #         self.str += str(node.key) + '\t'
+    #     self.str += "\n"
+    #     for node in self.get_list():
+    #         self.str += str(node.val) + '\n'
+    #
+    #     return self.str
+
+
+class HashMap:
+    def __init__(self, capacity=1, load_factor=1):
+        self.capacity = capacity
+        self.load_factor = load_factor
+        self.headers = [LinkedList() for _ in range(capacity)]
+        self.str = ""
+
+    def get_hash_key(self, key):
+        return hash(key) & (self.capacity - 1)
+
+    def put(self, key, val):
+        hash_key = self.get_hash_key(key)
+        linked_list = self.headers[hash_key]
+        if linked_list.size >= self.load_factor * self.capacity:
+            self.reset()
+            hash_key = self.get_hash_key(key)
+            linked_list = self.headers[hash_key]
+        node = linked_list.get_by_key(key)
+        if node is not None:
+            node.val = val
+        else:
+            node = Node(key, val)
+            linked_list.append(node)
+
+    def get(self, key):
+        hash_key = self.get_hash_key(key)
+        linked_list = self.headers[hash_key]
+        node = linked_list.get_by_key(key)
+        return node.val if node is not None else None
+
+    def delete(self, key):
+        node = self.get(key)
+        if node is None:
+            return False
+        hash_key = self.get_hash_key(key)
+        linked_list = self.headers[hash_key]
+        linked_list.delete(node)
+        return True
+
+    def reset(self):
+        headers = [LinkedList() for _ in range(self.capacity * 2)]
+        cap = self.capacity
+        self.capacity = self.capacity * 2
+        for i in range(cap):
+            linked_list = self.headers[i]
+            nodes = linked_list.get_list()
+            for u in nodes:
+                hash_key = self.get_hash_key(u.key)
+                head = headers[hash_key]
+                head.append(u)
+        self.headers = headers
+        self.str = ""
+
+    def to_string(self):
+        for list in self.headers:
+            for node in list.get_list():
+                self.str += str(node.key) + ":" + str(node.val) + "  "
+
+        return self.str
+
+    def sort(self):
+
         pass
--- suggest.py
+++ suggest.py
@@ -1,7 +1,8 @@
 # -*- coding: UTF-8 -*-
 import pandas as pd
 import common as m
-import numpy
+from common import HashMap, LinkedList, Node
+import numpy as np
 import common as m
 from sklearn.datasets import load_iris
 
@@ -13,9 +14,7 @@ CSV_4_0 = "data/statistics_4_red.csv"
 CSV_3_1 = "data/statistics_3_red_1_blue.csv"
 CSV_4_1 = "data/statistics_4_red_1_blue.csv"
 CSV_5_0 = "data/statistics_5_red.csv"
-MAX_SIZE = 30
-
-#
+MAX_SIZE = 20
 
 # 五等奖
 # 四等奖
@@ -34,43 +33,130 @@ def second_prize():
 # 三等奖
 def third_prize():
     m.log("5个红球 + 1个蓝球")
-    # print_old_data(CSV_5_1)
+    # load_old_data(CSV_5_1)
     pass
 
 
 # 四等奖
 def fourth_prize():
     m.log("5个红球")
-    print_old_data(CSV_5_0)
+    load_old_data(CSV_5_0)
     m.log("4个红球 + 1个蓝球")
-    print_old_data(CSV_4_1)
+    load_old_data(CSV_4_1)
 
 
 # 五等奖
 def fifth_prize():
     m.log("4个红球")
-    print_old_data(CSV_4_0)
+    load_old_data(CSV_4_0)
     m.log("3个红球 + 1个蓝球")
-    print_old_data(CSV_3_1)
+    load_old_data(CSV_3_1)
 
 
 # 打印曾经中奖的组合数据
-def print_old_data(csv):
+def load_old_data(csv):
     data = pd.read_csv(csv, encoding='utf-8', engine='python')
     data = data.sort_values(by=COL_SORT, ascending=False).head(MAX_SIZE)
     print(data.to_string())
     # 写入缓存用于后续分析
     CACHE[csv] = data
+    return data
 
 
 # 第1步：找出概率最高的中奖组合
 def step_1():
-    fifth_prize()
-    fourth_prize()
+    data_4_0 = load_old_data(CSV_4_0)
+    data_3_1 = load_old_data(CSV_3_1)
+    m.log("推荐组合如下")
+
+    for line in data_4_0.itertuples():
+        a = line[1]
+        b = line[2]
+        c = line[3]
+        d = line[4]
+        x, y = last_2_numbers(data_4_0, line[0], a, b, c, d)
+        m.print_r(a, b, c, d, x, y)
+
+    for line in data_3_1.itertuples():
+        a = line[1]
+        b = line[2]
+        c = line[3]
+        d = line[4]
+        x, y, z = last_3_numbers(data_3_1, line[0], a, b, c)
+        m.print_r(a, b, c, x, y, z)
 
 
-# 第2步：
-def step_2():
+# 推荐2个红球
+def last_2_numbers(data, idx, a, b, c, d):
+    x = 0
+    buf = np.zeros(m.MAX_RED_NUM - m.MIN_RED_NUM + 1, dtype=int, order='C')
+    for line in data.itertuples():
+        if line.Index != idx:
+            arr = [line.RED1, line.RED2, line.RED3, line.RED4]
+            i = 0
+            while i <= m.MAX_RED_NUM - m.MIN_RED_NUM:
+                if i != a and i != b and i != c and i != d and i in arr:
+                    buf[i] = int(buf[i] + 1)
+                i += 1
+    x = np.argmax(buf)
+
+    y = 0
+    buf = np.zeros(m.MAX_RED_NUM - m.MIN_RED_NUM + 1, dtype=int, order='C')
+    for line in data.itertuples():
+        if line.Index != idx:
+            arr = [line.RED1, line.RED2, line.RED3, line.RED4]
+            i = 0
+            while i <= m.MAX_RED_NUM - m.MIN_RED_NUM:
+                if i != a and i != b and i != c and i != d and i != x and i in arr:
+                    buf[i] = int(buf[i] + 1)
+                i += 1
+    y = np.argmax(buf)
+
+    return x, y
+
+
+def last_3_numbers(data, idx, a, b, c):
+    x = 0
+    buf = np.zeros(m.MAX_RED_NUM - m.MIN_RED_NUM + 1, dtype=int, order='C')
+    for line in data.itertuples():
+        if line.Index != idx:
+            arr = [line.RED1, line.RED2, line.RED3]
+            i = 0
+            while i <= m.MAX_RED_NUM - m.MIN_RED_NUM:
+                if i != a and i != b and i != c and i in arr:
+                    buf[i] = int(buf[i] + 1)
+                i += 1
+    x = np.argmax(buf)
+
+    y = 0
+    buf = np.zeros(m.MAX_RED_NUM - m.MIN_RED_NUM + 1, dtype=int, order='C')
+    for line in data.itertuples():
+        if line.Index != idx:
+            arr = [line.RED1, line.RED2, line.RED3]
+            i = 0
+            while i <= m.MAX_RED_NUM - m.MIN_RED_NUM:
+                if i != a and i != b and i != c and i != x and i in arr:
+                    buf[i] = int(buf[i] + 1)
+                i += 1
+    y = np.argmax(buf)
+
+    z = 0
+    buf = np.zeros(m.MAX_RED_NUM - m.MIN_RED_NUM + 1, dtype=int, order='C')
+    for line in data.itertuples():
+        if line.Index != idx:
+            arr = [line.RED1, line.RED2, line.RED3]
+            i = 0
+            while i <= m.MAX_RED_NUM - m.MIN_RED_NUM:
+                if i != a and i != b and i != c and i != x and i in arr:
+                    buf[i] = int(buf[i] + 1)
+                i += 1
+    z = np.argmax(buf)
+
+    return x, y, z
+
+
+def blue_ball():
+
     pass
 
 
